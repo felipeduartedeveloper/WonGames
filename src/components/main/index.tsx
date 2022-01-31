8da3128865a2c63e9b913c1792a850fde3363b70
felipeduartedelevoper <felipesilvapac@gmail.com>
2022-01-31 16:59:38 -0300

    Style main images

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,7 +2,18 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Wrapper>
-    <h1>React avançado</h1>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs, Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
   </S.Wrapper>
 )
 
